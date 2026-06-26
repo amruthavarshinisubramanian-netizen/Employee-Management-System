cf4a9b06b175e836e3e74b442403f8c4b4b2e74e
Amruthavarshini S <amruthavarshinisubramanian@gmail.com>
2026-06-26 14:55:40 +0530

    Update CORS policy to allow all origins

--- backend/src/main/java/backend/controller/EmployeeController.java
+++ backend/src/main/java/backend/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
     @Autowired
